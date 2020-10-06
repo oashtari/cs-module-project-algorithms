0896b9c811081a856c0ff6bb5d81d779ad51a27a
Omid Ashtari <oashtari@gmail.com>
2020-10-05 17:31:33 -0700

    initial commit with some class notes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,8 +2,29 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+# input: arr of numbers where thre is one number that is not a duplicate; every other number has a dupe
 def single_number(arr):
     # Your code here
+    # O(n**2)
+    for num in arr: # O(n)
+        if arr.count(num) == 1: # O(n)
+            return num
+
+
+def single_number(arr):
+    # sets are closely related cousing to dicts
+    # do not associate values with keys
+    # useful when you need the uniqueness properties of dicts
+    s = set()
+
+    for num in arr: #O(n)
+        if num in s: # O(1)
+            s.remove(num)
+        else:
+            s.add(num) #O(1)
+    # at this point, only element in set is our odd element out
+    return list(s)[0] #O(1)
 
     pass
 
