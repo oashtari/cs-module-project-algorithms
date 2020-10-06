626e6bf248413f956feed6487ed17eb1e9877786
Omid Ashtari <oashtari@gmail.com>
2020-10-06 05:51:32 -0700

    finished single number, added knapsack solution from class as a file

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,18 +2,21 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+import time
 
-# input: arr of numbers where thre is one number that is not a duplicate; every other number has a dupe
+# input: arr of numbers where thre is one number that is not a duplicate; 
+# every other number has a dupe
+# def single_number(arr):
+#     # Your code here
+#     # O(n**2)
+
+#     for num in arr: # O(n)
+#         if arr.count(num) == 1: # O(n)
+#             return num
+
+
 def single_number(arr):
-    # Your code here
-    # O(n**2)
-    for num in arr: # O(n)
-        if arr.count(num) == 1: # O(n)
-            return num
-
-
-def single_number(arr):
-    # sets are closely related cousing to dicts
+    # sets are closely related cousin to dictionaries
     # do not associate values with keys
     # useful when you need the uniqueness properties of dicts
     s = set()
@@ -26,11 +29,11 @@ def single_number(arr):
     # at this point, only element in set is our odd element out
     return list(s)[0] #O(1)
 
-    pass
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
+    start = time.time()
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
-
+    print(f'\nResult calculated in {time.time()-start:.20f} seconds')
     print(f"The odd-number-out is {single_number(arr)}")
