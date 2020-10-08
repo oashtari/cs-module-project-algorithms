9bb313a443107d2e79e701585d8c493aabdf620b
Omid Ashtari <oashtari@gmail.com>
2020-10-08 11:37:10 -0700

    made sure product all numbers is optimized, at least it seems to be

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -7,16 +7,16 @@ from functools import reduce
 def product_of_all_other_numbers(arr):
     # # Your code here
     sum = 1
+
+    # NAIVE 
     # new_arr = arr
     # for i in arr:
     #     sum *= i
     # final = [sum//i for i in arr]
     # return final
     
-    for i in arr:
-        sum = sum * i 
-    return [sum/i for i in arr]
 
+    # NOT WORKING YET, LIKELY TOO SLOW ANYWAY
     # for i in range(len(arr)):
     #     for j in range(len(arr)):
     #         print('j', arr[j])  
@@ -27,6 +27,10 @@ def product_of_all_other_numbers(arr):
     # # sum = sum/new_arr[i]
     # return sum/arr[i]
 
+    # OPTIMIZED
+    for i in arr:
+        sum = sum * i 
+    return [sum/i for i in arr]
 
 
 
@@ -40,7 +44,7 @@ def product_of_all_other_numbers(arr):
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [4, 2, 3, 1, 5]
-    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    # arr = [4, 2, 3, 1, 5]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
